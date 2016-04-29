4431d3626ce267272d4cc1fd21cef1bdd2ef7092
cjn1767 <cjn1767@lhup.edu>
2016-04-29 19:10:00 -0400

    First Commit

---
+++ app/src/main/java/student/application/lhu_companion_2/models/Events.java
@@ -0,0 +1,61 @@
+package student.application.lhu_companion_2.models;
+
+import java.util.Date;
+
+
+public class Events {
+
+    String eventName, host, location, time, description;
+    Date when;
+
+
+    public String getEventName() {
+        return eventName;
+    }
+
+    public void setEventName(String eventName) {
+        this.eventName = eventName;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String host) {
+        this.location = location;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getWhen() {
+        return when;
+    }
+
+    public void set(Date when) {
+        this.when = when;
+    }
+
+
+}
